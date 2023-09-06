4af03e14d04849e43c7c6e03b8665b0e28ddf237
Ryan Shore <mshorecode@gmail.com>
2023-09-05 23:46:29 -0500

    Started creating api calls

---
+++ api/vocabularyData.js
@@ -0,0 +1,39 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+const getVocabulary = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'applications/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const createVocabulary = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getVocabulary,
+  createVocabulary,
+};
