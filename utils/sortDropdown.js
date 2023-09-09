da7437e90397c2f0c4dc47c68bb4498cb532364e
Ryan Shore <mshorecode@gmail.com>
2023-09-09 10:57:26 -0500

    Fixed sort alphabetically

--- utils/sortDropdown.js
+++ utils/sortDropdown.js
@@ -1,6 +1,6 @@
 const sortAscending = (a, b) => a.timeSubmitted.localeCompare(b.timeSubmitted);
 const sortDescending = (a, b) => b.timeSubmitted.localeCompare(a.timeSubmitted);
-const sortAlphabetically = (a, b) => a.title < b.title;
+const sortAlphabetically = (a, b) => a.title > b.title;
 
 export {
   sortAscending,
